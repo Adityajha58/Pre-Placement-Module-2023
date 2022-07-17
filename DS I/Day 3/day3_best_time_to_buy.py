12c9624626cfa7ae133494acbc312bc358c4568f
Aditya <adityajha5868@gmail.com>
2022-07-17 23:44:10 +0530

    Create day3_best_time_to_buy.py

---
+++ DS I/Day 3/day3_best_time_to_buy.py
@@ -0,0 +1,6 @@
+max_profit = 0
+        min_buy = float('inf')
+        for price in prices:
+            min_buy=min(min_buy, price)
+            max_profit=max(max_profit, price-min_buy)
+        return max_profit
