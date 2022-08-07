e722a7d82ecbcf9441de252e3a39e4df6aa97756
Aditya <adityajha5868@gmail.com>
2022-08-07 18:41:01 +0530

    Create day5_product_of_array_except_self.py

---
+++ DS II/Day 5/day5_product_of_array_except_self.py
@@ -0,0 +1,19 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        """Calculate an array of the products for every element
+        in the array except the one at the same index.
+        Args:
+            nums (List[int]): Array of ints
+        Returns:
+            List[int]: Array of products
+        """
+        result = []
+        for i in range(len(nums)):
+            prefix_product = 1
+            suffix_product = 1
+            for pre in nums[:i]:
+                prefix_product *= pre
+            for suf in nums[i + 1 :]:
+                suffix_product *= suf
+            result.append(prefix_product * suffix_product)
+        return result
